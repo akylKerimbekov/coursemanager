0ed411d100153f012e66042f2486428c57dda504
Akyl Kerimbekov <akyl.kerimbekov@gmail.com>
2019-02-03 16:51:38 +0600

    init project

---
+++ src/test/java/tech/akyl/courses/coursemanager/CoursemanagerApplicationTests.java
@@ -0,0 +1,17 @@
+package tech.akyl.courses.coursemanager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CoursemanagerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
