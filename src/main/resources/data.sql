c0d7ac272185d1e86072469f62bc67fa644df856
Akyl Kerimbekov <akyl.kerimbekov@gmail.com>
2019-02-07 23:12:26 +0600

    populate with sample data teacher entity

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into teacher values (1, 'Aifer', 'Altmyshbaeva');
+insert into teacher values (2, 'Zarina', 'Izmailova');
+insert into teacher values (3, 'Rinata', 'Izmailova');
+insert into teacher values (4, 'Aijan', 'Kerimbekova');
+insert into teacher values (5, 'Nurjan', 'Kerimbekova');
+insert into teacher values (6, 'Janyl', 'Altmyshbaeva');
