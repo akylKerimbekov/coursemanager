2b7b0ca69aa59be9a00c7be96afca7418f7d65f7
Akyl Kerimbekov <akyl.kerimbekov@gmail.com>
2019-02-09 08:12:16 +0600

    fix typo on teacher.html which caused problim with id

--- src/main/java/tech/akyl/courses/coursemanager/controller/TeacherController.java
+++ src/main/java/tech/akyl/courses/coursemanager/controller/TeacherController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import tech.akyl.courses.coursemanager.data.entity.Teacher;
 import tech.akyl.courses.coursemanager.data.repository.TeacherRepository;
 
 @Controller
